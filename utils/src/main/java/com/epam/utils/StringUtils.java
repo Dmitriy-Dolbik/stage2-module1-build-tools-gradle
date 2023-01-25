eeba6cd67bae823e4c361ac478e4b82f373c0e2a
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-25 23:38:57 +0300

    Correct solution4

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,7 +2,7 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if(str.equals("null") || str == "" || str == " ")
+        if(str.equals("null") || str == "" || str == " " || str == null || str.startsWith("0"))
         {
             return false;
         } else {
