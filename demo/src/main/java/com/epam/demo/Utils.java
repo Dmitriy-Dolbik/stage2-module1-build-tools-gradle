43717db5cd66e41f32c40ad4b1d3ee4f2d461f91
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-25 22:48:41 +0300

    Correct solution

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,16 +1,22 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.epam.utils.StringUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for(String arg : args){
-               if (!StringUtils.isPositiveNumber(arg)){
-                return false;
+        if(args == null && args.size() == 0){
+            return false;
+        } else {
+            for(String arg : args){
+                if (!StringUtils.isPositiveNumber(arg)){
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,12 +2,18 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        double number = Double.parseDouble(str);
-         if (number > 0){
-             return true;
-         }
-         else {
-             return false;
-         }
+        if(str == null && str == "")
+        {
+            return false;
+        } else {
+            double number = Double.parseDouble(str);
+            if (number > 0){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
     }
 }
